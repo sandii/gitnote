9d2537a824f556c27c95cddc9678fe280bb2604f
sandii <chenzhibupt@qq.com>
2017-02-22 22:30:08 +0800

    update

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,12 @@
-import fetch from 'util/fetch';
-import AsideList from 'component/aside-list';
+import fetch from './util/fetch';
+import AsideList from './component/aside-list';
 
 class Main extends React.Component {
 
 	constructor (props) {
 		super(props);
 		this.state = {
-			pathArr : [],
-
+			pathArr : ['root'],
 			dirList : null,
 			// dirList : [{
 			// 	type : 'repo',
@@ -25,11 +24,19 @@ class Main extends React.Component {
 	getDirTree () {
 		fetch.get('../data/dir-tree.json', {}).then(dirTree => {
 			this.dirTree = dirTree;
-			this.updateDirList(this.dirTree.root);
+			this.updateDirList(this.getDirObj());
 		});
 	}
 
-	updateDirList (obj) {
+	getDirObj () {
+		let obj = this.dirTree;
+		this.state.pathArr.forEach(name => {
+			obj = obj[name];
+		});
+		return obj;
+	}
+
+	updateDirList (dirObj) {
 		let dirList = [];
 		let isPic = name => {
 			let lower = name.toLowerCase();
@@ -37,51 +44,67 @@ class Main extends React.Component {
 			if (lower.includes('.png')) return true;
 			return false;
 		};
-		for (let name in obj) {
-			let type = obj[name] ? 'dir' : 'file';
+		for (let name in dirObj) {
+			let type = dirObj[name] ? 'dir' : 'file';
 			type = type === 'file' && isPic(name) ? 'pic' : type;
 			dirList.push({ type, name });
 		}
+		this.setState({ dirList });
 
 	}
 
 	changeChecked (index) { this.setState({ index }); }
 
-	forward (index) {}
+	forward (index) {
+		let name = this.state.dirList[index].name;
+		let pathArr = this.state.pathArr.concat([name]);
+		this.setState({ pathArr });
+		this.updateDirList(this.getDirObj());
+	}
+
+	back () {
+		let len = this.state.pathArr.length;
+		if (len <= 1) return;
+		let pathArr = this.state.pathArr.slice(0, len - 2);
+		this.setState({ pathArr });
+		this.updateDirList(this.getDirObj());
+	}
 
 	cat (index) {}
 
 	render () {
 		return (
-			<section className="full">
-				<header>
-					<a className="icon" target="_blank" href="https://github.com/sandii"></a>
-				</header>
-				<aside>
-					<ul className="aside-ctrl">
-						<li className="back"></li>
-						<li className="sync"></li>
-						<li className="add"></li>
-						<li className="rename"></li>
-						<li className="del"></li>
-						<li className="edit"></li>
-					</ul>
-					<AsideList 
-						data={this.state.dirList}
-						index={this.state.index}
-						forward={this.forward.bind(this)}
-						cat={this.cat.bind(this)}
-						changeChecked={this.changeChecked.bind(this)} />
-				</aside>
-				<nav>
-					/ <span>root</span>
-					/ <span>repo</span>
-					/ <span>dir</span>
-					/ <span>file</span>
-					/
-				</nav>
-				<article></article>
-			</section>
+<section className="full">
+	<header>
+		<a className="icon" target="_blank" href="https://github.com/sandii"></a>
+	</header>
+	<aside>
+		<ul className="aside-ctrl">
+			<li 
+				className="back"
+				onClick={this.back.bind(this)}></li>
+			<li className="sync"></li>
+			<li className="add"></li>
+			<li className="rename"></li>
+			<li className="del"></li>
+			<li className="edit"></li>
+		</ul>
+		<AsideList 
+			data={this.state.dirList}
+			index={this.state.index}
+			forward={this.forward.bind(this)}
+			cat={this.cat.bind(this)}
+			changeChecked={this.changeChecked.bind(this)} />
+	</aside>
+	<nav>
+		/ <span>root</span>
+		/ <span>repo</span>
+		/ <span>dir</span>
+		/ <span>file</span>
+		/
+	</nav>
+	<article></article>
+</section>
 		);
 	}
 };
